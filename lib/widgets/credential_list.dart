c941f7b66a7bf3d36b03b12b0c7e25b5998c9472
Jacob White <jacobewhitedev@gmail.com>
2022-01-31 16:29:22 -0600

    added styling to list items

--- lib/widgets/credential_list.dart
+++ lib/widgets/credential_list.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/credential_provider.dart';
 
@@ -8,15 +8,45 @@ class CredentailList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var credentialArr = Provider.of<CredentialProvider>(context).credentialArr;
-    print(credentialArr[0]['url']);
 
     return Expanded(
       child: ListView.builder(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
         itemCount: credentialArr.length,
         itemBuilder: (context, i) {
-          return Image(
-            image: NetworkImage("${credentialArr[i]['url']}/favicon.ico"),
+          return Container(
+            margin: const EdgeInsets.only(bottom: 15),
+            child: Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: const Color(0xff121212),
+                  ),
+                  padding: const EdgeInsets.all(5),
+                  margin: const EdgeInsets.only(right: 20),
+                  child: Image(
+                    height: 35,
+                    image:
+                        NetworkImage("${credentialArr[i]['url']}/favicon.ico"),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      credentialArr[i]['service'],
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      credentialArr[i]['username'],
+                      style:
+                          TextStyle(fontSize: 14, color: Colors.grey.shade700),
+                    ),
+                  ],
+                )
+              ],
+            ),
           );
         },
       ),
