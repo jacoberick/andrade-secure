ebc9b3648f27fd717b90779bffcdadaba37964a6
Jacob White <jacobewhitedev@gmail.com>
2022-02-01 09:41:53 -0600

    added ellipsis to list items, positioned list item elements

--- lib/widgets/credential_list.dart
+++ lib/widgets/credential_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_gifs/loading_gifs.dart';
 import 'package:provider/provider.dart';
@@ -18,37 +19,50 @@ class CredentailList extends StatelessWidget {
           return Container(
             margin: const EdgeInsets.only(bottom: 15),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  height: 35,
-                  width: 35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    color: const Color(0xfffefefe),
-                  ),
-                  padding: const EdgeInsets.all(5),
-                  margin: const EdgeInsets.only(right: 20),
-                  child: Center(
-                    child: FadeInImage.assetNetwork(
-                        placeholder: cupertinoActivityIndicator,
-                        image: "${credentialArr[i]['url']}/favicon.ico"),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                Row(
                   children: [
-                    Text(
-                      credentialArr[i]['service'],
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xfffefefe)),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: const Color(0xfffefefe),
+                      ),
+                      padding: const EdgeInsets.all(5),
+                      margin: const EdgeInsets.only(right: 20),
+                      child: Center(
+                        child: FadeInImage.assetNetwork(
+                            placeholder: cupertinoActivityIndicator,
+                            image: "${credentialArr[i]['url']}/favicon.ico"),
+                      ),
                     ),
-                    Text(
-                      credentialArr[i]['username'],
-                      style:
-                          TextStyle(fontSize: 14, color: Colors.grey.shade400),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          credentialArr[i]['service'],
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xfffefefe)),
+                        ),
+                        Text(
+                          credentialArr[i]['username'],
+                          style: TextStyle(
+                              fontSize: 14, color: Colors.grey.shade400),
+                        ),
+                      ],
                     ),
                   ],
+                ),
+                const CupertinoButton(
+                  padding: EdgeInsets.only(left: 16),
+                  child: Icon(
+                    CupertinoIcons.ellipsis,
+                    color: Colors.white,
+                  ),
+                  onPressed: null,
                 )
               ],
             ),
